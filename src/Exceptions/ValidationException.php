0f3c2f518cf86789039882cb6131dad7afea4269
Guilherme <havennow@gmail.com>
2017-11-30 11:17:58 -0200

    Testing

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -14,6 +14,7 @@ class ValidationException extends RequestException
             var_dump($errors);
             exit;
         }
+        
         parent::__construct($status, $errors, $lastOptions);
         $message = "Erros de validação foram encontrados!";
         $firstError = reset($errors);
