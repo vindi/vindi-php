62d86777bee3bb5778eb6700a5b714da211dac41
Tauan Lemos <tauan.lemos@convenia.com.br>
2018-03-13 10:08:09 -0300

    Add retry to invoices

--- tests/InvoiceTest.php
+++ tests/InvoiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Vindi\Test;
 
+use stdClass;
 use Vindi\ApiRequester;
 use Vindi\Invoice;
 
@@ -17,5 +18,17 @@ class InvoiceTest extends ResourceTest
     public function it_should_have_an_endpoint()
     {
         $this->assertSame($this->resource->endpoint(), 'invoices');
+    }
+
+    /** @test */
+    public function it_should_retry_an_invoice()
+    {
+        $stdClass = new stdClass;
+
+        $this->resource->apiRequester->method('request')->willReturn($stdClass);
+
+        $response = $this->resource->retry(1);
+
+        $this->assertSame($response, $stdClass);
     }
 }
