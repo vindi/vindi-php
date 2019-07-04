81111d1500675bf480fe3da74be7979e5790429f
victorsinged <50752933+victorsinged@users.noreply.github.com>
2019-07-04 15:58:51 -0300

    Adiciona endpoint de movements (#53)

---
+++ src/Movement.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vindi;
+
+/**
+ * Class Movement
+ *
+ * @package Vindi
+ */
+class Movement extends Resource
+{
+    /**
+     * The endpoint that will hit the API.
+     *
+     * @return string
+     */
+    public function endPoint()
+    {
+        return 'movements';
+    }
+}
---
+++ tests/MovementTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vindi\Test;
+
+use Vindi\Movement;
+use Vindi\ApiRequester;
+
+class MovementTest extends ResourceTest
+{
+    public function setUp()
+    {
+        $this->resource = $this->getMockForAbstractClass(Movement::class);
+        $this->resource->apiRequester = $this->getMock(ApiRequester::class);
+    }
+
+    /** @test */
+    public function it_should_have_an_endpoint()
+    {
+        $this->assertSame($this->resource->endpoint(), 'movements');
+    }
+}
